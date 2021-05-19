094c146aca0ed2cdabf36ae6f1bf1f70b79d3883
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-19 11:40:39 -0600

    Added basic component in NewPlaceScreen

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const NewPlaceScreen = () => {
+    return (
+        <View>
+            <Text>NEW PLACE</Text>
+        </View>
+    )
+}
+
+export default NewPlaceScreen
