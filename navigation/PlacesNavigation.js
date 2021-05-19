4b45e05b93da55f5b8d14ffd92d81cee26f23666
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-19 11:27:30 -0600

    Added necesaries libraries and components

---
+++ navigation/PlacesNavigation.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer  } from '@react-navigation/native'
+
+import MapScreen from '../screens/MapScreen'
+import NewPlaceScreen from '../screens/NewPlaceScreen'
+import PlaceDetailScreen from '../screens/PlaceDetailScreen'
+import PlacesListScreen,{placesOptions} from '../screens/PlacesListScreen'
